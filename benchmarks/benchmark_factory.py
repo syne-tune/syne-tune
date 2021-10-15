089fb2dcd5d3a3be4a88ba518cf6e7c10a800be3
David Salinas <dsalina@amazon.com>
2021-10-15 15:49:35 +0200

    add license header

--- benchmarks/benchmark_factory.py
+++ benchmarks/benchmark_factory.py
@@ -1,3 +1,15 @@
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
 from examples.training_scripts.mlp_on_fashion_mnist.mlp_on_fashion_mnist \
     import mlp_fashionmnist_benchmark, mlp_fashionmnist_default_params
 from examples.training_scripts.nasbench201.nasbench201 import \
