8372661245990fe9e4a9ccf1c269e7e40588f9f8
David Salinas <dsalina@amazon.com>
2021-10-15 15:50:37 +0200

    add copyright script

---
+++ license-add.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+add_license_folder() {
+	folder=$1
+	echo "adding license to folder $folder"
+	for i in $(find $folder -name '*.py');
+	do
+	  if ! grep -q Copyright $i
+	  then
+	  	echo "adding license to $i"
+	    cat copyright.txt $i > $i.new && mv $i.new $i
+	  fi
+	done
+}
+
+add_license_folder "sagemaker_tune/"
+add_license_folder "examples/"
+add_license_folder "benchmarks/"
+add_license_folder "tst/"
+
