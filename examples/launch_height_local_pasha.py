798f8e08e0c90279b994ea232542febbdcfc2124
Matthias Seeger <matthis@amazon.de>
2021-10-25 10:55:37 +0200

    Comments David

--- examples/launch_height_local_pasha.py
+++ examples/launch_height_local_pasha.py
@@ -1,3 +1,15 @@
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
 import logging
 from pathlib import Path
 
@@ -52,7 +64,7 @@ if __name__ == '__main__':
         epsilon_scaling=epsilon_scaling
         )
 
-    stop_criterion = StoppingCriterion(max_num_trials_started=30)
+    stop_criterion = StoppingCriterion(max_wallclock_time=60)
     
     tuner = Tuner(
         backend=backend,
