49baf5f62598d49be0a51bacac949555013fec99
David Salinas <geoalgo@users.noreply.github.com>
2021-11-15 13:19:35 +0100

    remove shell mode in get_num_gpus() and simplify util (#32)
    * remove shell mode in get_num_gpus() and simplify util
    
    * remove print
    
    Co-authored-by: David Salinas <dsalina@amazon.com>

--- sagemaker_tune/num_gpu.py
+++ sagemaker_tune/num_gpu.py
@@ -11,96 +11,44 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 """
-Taken from
+Adapted from to not run in Shell mode which is unsecure.
 https://github.com/aws/sagemaker-rl-container/blob/master/src/vw-serving/src/vw_serving/sagemaker/gpu.py
 """
 
+import logging
 import subprocess
 import time
-from multiprocessing import TimeoutError
-import logging
 
-
-AUTODETECT_GPU_COUNT = "auto"
 _num_gpus = None
 
 
-def _query_num_gpus():
-    """
-    Returns the number of GPU devices on the host. Returns 0 if the host has no GPU devices.
-    """
-
-    global _num_gpus
-    if _num_gpus is None:
-        COMMAND = 'nvidia-smi -L 2>/dev/null | grep \'GPU [0-9]\' | wc -l'
-        TIMEOUT_SECONDS = 75
-        STATUS_POLL_INTERVAL_SECONDS = 0.025
-
-        try:
-            # todo remove shell mode
-            proc = subprocess.Popen(COMMAND, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, bufsize=1)
-        except (OSError, ValueError):
-            logging.exception("Error launching /usr/bin/nvidia-smi.")
-            return 0
-
-        start_time = time.time()
-
-        # Wait for the process to finish
-        exitcode = None
-        while exitcode is None and time.time() - start_time < TIMEOUT_SECONDS:
-            time.sleep(STATUS_POLL_INTERVAL_SECONDS)
-            exitcode = proc.poll()
-
-        # Terminate the process if not finished
-        if exitcode is None:
-            logging.error("nvidia-smi timed out after %s secs", time.time() - start_time)
-            proc.terminate()
-            raise TimeoutError
-
-        _num_gpus = int(proc.stdout.readline())
-        # logging.info("nvidia-smi took: %s secs to identify %d gpus", time.time() - start_time, _num_gpus)
-
-    return _num_gpus
-
-
-def get_num_gpus(num_gpus=AUTODETECT_GPU_COUNT, **kwargs) -> int:
+def get_num_gpus() -> int:
     """
     Returns the number of available GPUs based on configuration parameters and available hardware GPU devices.
-
-    :param num_gpus: (int or "auto")
-        If set to "auto", the function queries and returns the number of available GPUs.
-        If set to an integer value, the function returns the value of min(num_gpus, auto_detected_gpu_count)
-        Otherwise raises ValueError.
-    :param kwargs: additional configuration parameters, not used
+    Gpus are detected by running "nvidia-smi --list-gpus" as a subprocess.
     :return: (int) number of GPUs
     """
-
-    # Shortcut execution if what we want is 0 gpu, i.e. only cpu
-    if num_gpus == 0:
-        return 0
-
-    try:
-        num_available_gpus = _query_num_gpus()
-    except TimeoutError:
-        if num_gpus == AUTODETECT_GPU_COUNT:
-            return 0
-        else:
-            return num_gpus
-
-    if num_gpus == AUTODETECT_GPU_COUNT:
-        return num_available_gpus
-    else:
+    global _num_gpus
+    if _num_gpus is None:
         try:
-            num_requested_gpus = int(num_gpus)
-        except ValueError:
-            raise ValueError(
-                "Invalid value '{}' provided for hyperparameter '_num_gpus'. '_num_gpus' must be an integer or 'auto'. "
-                "Please set the value of '_num_gpus' hyperparameter to 'auto' or an integer value and try again."
-                .format(num_gpus))
+            cmd = "nvidia-smi --list-gpus"
+            with open("std.out", 'w') as stdout:
+                proc = subprocess.Popen(cmd.split(" "), shell=False, stdout=stdout)
+            max_trials = 0
+            while proc.poll() is None and max_trials < 100:
+                time.sleep(0.1)
+                max_trials += 1
 
-        if num_requested_gpus > num_available_gpus:
-            logging.warning("Request number of gpus: %d, Number of GPUs found: %d",
-                            num_requested_gpus, num_available_gpus)
-            return num_available_gpus
-        else:
-            return num_requested_gpus
+            if proc.poll() is None:
+                raise ValueError("nvidia-smi timed out after 10 secs.")
+
+            with open("std.out", 'r') as stdout:
+                _num_gpus = len(stdout.readlines())
+            return _num_gpus
+
+        except (OSError, FileNotFoundError):
+            logging.info("Error launching /usr/bin/nvidia-smi, no GPU could be detected.")
+            _num_gpus = 0
+            return 0
+    else:
+        return _num_gpus
