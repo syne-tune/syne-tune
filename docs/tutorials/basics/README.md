0f110400fd6fd45895ad8eda4091dbab66ccfa64
Matthias Seeger <matthis@amazon.de>
2022-01-18 10:43:22 +0100

    Changes recommended by David

--- docs/tutorials/basics/README.md
+++ docs/tutorials/basics/README.md
@@ -5,6 +5,17 @@ This tutorial provides an overview of Syne Tune. You will learn about the most
 important concepts of automated hyperparameter tuning, and how to make it work
 for your setup.
 
+If you like to run the code linked in this tutorial, please make sure to use
+the current `PyPI` release. If you cloned the source repository, this is
+obtained as follows:
+
+```bash
+git checkout -b basic_tutorial v0.11
+```
+
+This gives you a local branch `basic_tutorial`, in which you can play around
+with the code.
+
 
 # Table of Contents
 
--- docs/tutorials/basics/scripts/traincode_notune.py
+++ docs/tutorials/basics/scripts/traincode_notune.py
@@ -1,3 +1,26 @@
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+# If you like to run the code linked in this tutorial, please make sure to use
+# the current `PyPI` release. If you cloned the source repository, this is
+# obtained as follows:
+#
+# ```bash
+# git checkout -b basic_tutorial v0.11
+# ```
+#
+# This gives you a local branch `basic_tutorial`, in which you can play around
+# with the code.
 import os
 import argparse
 import logging
