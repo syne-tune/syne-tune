b7f248a9e8fa3ca28706312dc175dfd6269d51e5
David Salinas <dsalina@amazon.com>
2022-01-21 11:50:11 +0100

    RL cartpole example.

---
+++ examples/training_scripts/rl_cartpole/train_cartpole.py
@@ -0,0 +1,53 @@
+"""
+Adapts the introductionary example of rllib that trains a Cartpole with PPO.
+The input arguments learning-rate and gamma discount factor can be tuned for maximizing the episode mean reward.
+"""
+from argparse import ArgumentParser
+from ray.rllib.agents import DefaultCallbacks
+from syne_tune.report import Reporter
+
+
+class ReportResultsCallback(DefaultCallbacks):
+    def __init__(self):
+        super(ReportResultsCallback, self).__init__()
+        self.reporter = Reporter()
+
+    def on_train_result(self, *, trainer: "Trainer", result: dict, **kwargs) -> None:
+        self.reporter(**result)
+
+
+if __name__ == '__main__':
+
+    from ray.rllib.agents.ppo import PPOTrainer
+
+    parser = ArgumentParser()
+    parser.add_argument('--max_training_steps', type=int, default=100)
+    parser.add_argument('--lr', type=float, default=5e-5)
+    parser.add_argument('--gamma', type=float, default=0.99)
+    args, _ = parser.parse_known_args()
+
+    # Configure the algorithm.
+    config = {
+        # Environment (RLlib understands openAI gym registered strings).
+        "env": "CartPole-v0",
+        "num_workers": 2,
+        # Change this to "framework: torch", if you are using PyTorch.
+        # Also, use "framework: tf2" for tf2.x eager execution.
+        "framework": "tf",
+        "gamma": args.gamma,
+        "lr": args.lr,
+    }
+
+    trainer = PPOTrainer(config=config)
+
+    reporter = Reporter()
+    # Run it for n max_training_steps iterations. A training iteration includes
+    # parallel sample collection by the environment workers as well as
+    # loss calculation on the collected batch and a model update.
+    # Episode reward mean is reported each time.
+    for i in range(args.max_training_steps):
+        results = trainer.train()
+        reporter(
+            training_iter=i+1,
+            episode_reward_mean=results['episode_reward_mean'],
+        )
