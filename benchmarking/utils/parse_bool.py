616b3fc05da770de7b626551a7eb6d7f65834d3e
Aaron Klein <kleiaaro@amazon.com>
2022-01-25 10:13:36 +0100

    add missing files

---
+++ benchmarking/utils/parse_bool.py
@@ -0,0 +1,10 @@
+
+
+def parse_bool(val: str) -> bool:
+    val = val.upper()
+    if val == 'TRUE':
+        return True
+    else:
+        assert val == 'FALSE', \
+            f"val = '{val}' is not a boolean value"
+        return False
