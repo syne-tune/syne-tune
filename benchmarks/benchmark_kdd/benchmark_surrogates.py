ec6b1335d12d16c35f161a45ca79f47da1e0b4f8
David Salinas <dsalina@amazon.com>
2022-02-02 12:11:52 +0100

    default gaussian normalization

---
+++ benchmarks/benchmark_kdd/benchmark_surrogates.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import itertools
+import xgboost
+from sklearn.neighbors import KNeighborsRegressor
+from tqdm import tqdm
+
+from blackbox_repository import load
+from syne_tune.optimizer.transfer_learning import TransferLearningTaskEvaluations
+from syne_tune.optimizer.transfer_learning.quantile_based.thompson_sampling_functional_prior import \
+    extract_input_output, eval_model, fit_model
+
+
+def benchmark_surrogates():
+    # todo 2) roundrobin eval
+    # todo 3) compare different surrogates
+    blackbox_names = ["fcnet", "nasbench201"]
+    rows = []
+    surrogates = {
+        "xgboost": lambda: xgboost.XGBRegressor(),
+        "knn": lambda: KNeighborsRegressor(n_neighbors=3),
+    }
+    num_samples = [100, 1000, 10000, 100000, 1000000]
+    combinations = list(itertools.product(blackbox_names, surrogates.items(), num_samples))
+
+    print(combinations)
+    for blackbox_name, (surrogate_name, surrogate), max_fit_samples in tqdm(combinations):
+        if surrogate_name == "knn" and max_fit_samples > 10000:
+            continue
+        print(blackbox_name, surrogate_name, max_fit_samples)
+        bb_dict = load(blackbox_name)
+        normalization = "gaussian"
+        for test_task in bb_dict.keys():
+            config_space = bb_dict[test_task].configuration_space
+            metric_index = 0
+            transfer_learning_evaluations_train = {
+                task: TransferLearningTaskEvaluations(
+                    hyperparameters=bb.hyperparameters,
+                    # average over seed, take last fidelity and pick only first metric
+                    metrics=bb.objectives_evaluations.mean(axis=1)[:, -1, metric_index:metric_index + 1]
+                )
+                for task, bb in bb_dict.items()
+                if task != test_task
+            }
+            transfer_learning_evaluations_test = {
+                task: TransferLearningTaskEvaluations(
+                    hyperparameters=bb.hyperparameters,
+                    # average over seed, take last fidelity and pick only first metric
+                    metrics=bb.objectives_evaluations.mean(axis=1)[:, -1, metric_index:metric_index + 1]
+                )
+                for task, bb in bb_dict.items()
+                if task == test_task
+            }
+            model_pipeline, sigma_train, sigma_val = fit_model(
+                config_space=config_space,
+                transfer_learning_evaluations=transfer_learning_evaluations_train,
+                normalization=normalization,
+                max_fit_samples=max_fit_samples,
+                model=surrogate(),
+            )
+            X, y = extract_input_output(transfer_learning_evaluations_test, normalization)
+            sigma_test = eval_model(model_pipeline, X, y)
+            row = {
+                "task": test_task,
+                "RMSE-train": sigma_train,
+                "RMSE-val": sigma_val,
+                "RMSE-test": sigma_test,
+                "num_samples": max_fit_samples,
+                "surrogate": surrogate_name,
+            }
+            print(row)
+            rows.append(row)
+
+    df = pd.DataFrame(rows)
+    print(df.to_string())
+    df.to_csv("surrogates_results.csv", index=False)
+
+
+if __name__ == '__main__':
+    benchmark_surrogates()
+    df = pd.read_csv("surrogates_results.csv")
+    print(df.to_string())
+    df_pivot = df.pivot_table(index='task', columns=['surrogate', 'num_samples'], values='RMSE-test')
+    print(df_pivot.to_string(float_format="%.2f"))
--- syne_tune/optimizer/transfer_learning/quantile_based/thompson_sampling_functional_prior.py
+++ syne_tune/optimizer/transfer_learning/quantile_based/thompson_sampling_functional_prior.py
@@ -17,6 +17,59 @@ import pandas as pd
 from syne_tune.util import catchtime
 
 
+def extract_input_output(transfer_learning_evaluations, normalization: str):
+    X = pd.concat(
+        [evals.hyperparameters for evals in transfer_learning_evaluations.values()],
+        ignore_index=True
+    )
+    normalizer = from_string(normalization)
+    y = np.concatenate([
+        normalizer(evals.metrics).transform(evals.metrics)
+        for evals in transfer_learning_evaluations.values()
+    ], axis=0)
+    return X, y
+
+
+def fit_model(config_space, transfer_learning_evaluations, normalization: str, max_fit_samples: int, model=xgboost.XGBRegressor()):
+    model_pipeline = BlackboxSurrogate.make_model_pipeline(
+        configuration_space=config_space,
+        fidelity_space={},
+        model=model,
+    )
+    X, y = extract_input_output(transfer_learning_evaluations, normalization)
+    with catchtime("time to fit the model"):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+        X_train, y_train = _subsample(X_train, y_train, max_samples=max_fit_samples)
+        model_pipeline.fit(X_train, y_train)
+
+        # compute residuals (num_metrics,)
+        sigma_train = eval_model(model_pipeline, X_train, y_train)
+
+        # compute residuals (num_metrics,)
+        sigma_val = eval_model(model_pipeline, X_test, y_test)
+
+    return model_pipeline, sigma_train, sigma_val
+
+
+def eval_model(model_pipeline, X, y):
+    # compute residuals (num_metrics,)
+    mu_pred = model_pipeline.predict(X)
+    if mu_pred.ndim == 1:
+        mu_pred = mu_pred.reshape(-1, 1)
+    res = np.std(y - mu_pred, axis=0)
+    return res.mean()
+
+
+def _subsample(X_train, z_train, max_samples: int = 10000):
+    assert len(X_train) == len(z_train)
+    X_train.reset_index(inplace=True)
+    if max_samples is not None and max_samples < len(X_train):
+        random_indices = np.random.permutation(len(X_train))[:max_samples]
+        X_train = X_train.loc[random_indices]
+        z_train = z_train[random_indices]
+    return X_train, z_train
+
+
 class TS(BaseSearcher):
 
     def __init__(
@@ -26,45 +79,21 @@ class TS(BaseSearcher):
             metric: str,
             transfer_learning_evaluations: Dict[str, TransferLearningTaskEvaluations],
             max_fit_samples: int = 20000,
-            normalization: str = "standard",
+            normalization: str = "gaussian",
     ):
         super(TS, self).__init__(configspace=config_space, metric=metric)
         self.mode = mode
         # TODO option to pick only last fidelity
 
-        self.model_pipeline = BlackboxSurrogate.make_model_pipeline(
-            configuration_space=config_space,
-            fidelity_space={},
-            # model=KNeighborsRegressor(n_neighbors=3),
+        self.model_pipeline, sigma_train, sigma_val = fit_model(
+            config_space=config_space,
+            transfer_learning_evaluations=transfer_learning_evaluations,
+            normalization=normalization,
+            max_fit_samples=max_fit_samples,
             model=xgboost.XGBRegressor(),
         )
-        X = pd.concat(
-            [evals.hyperparameters for evals in transfer_learning_evaluations.values()],
-            ignore_index=True
-        )
-        normalizer = from_string(normalization)
-        y = np.concatenate([
-            normalizer(evals.metrics).transform(evals.metrics)
-            for evals in transfer_learning_evaluations.values()
-        ], axis=0)
-        with catchtime("time to fit the model"):
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
-            X_train, y_train = self._subsample(X_train, y_train, max_samples=max_fit_samples)
-            self.model_pipeline.fit(X_train, y_train)
-            # (num_hps, num_metrics)
-
-            # compute residuals (num_metrics,)
-            mu_train = self.model_pipeline.predict(X_train)
-            if mu_train.ndim == 1:
-                mu_train = mu_train.reshape(-1, 1)
-            print(f"residual train: {np.std(y_train - mu_train, axis=0)}")
-
-            # compute residuals (num_metrics,)
-            mu_test = self.model_pipeline.predict(X_test)
-            if mu_test.ndim == 1:
-                mu_test = mu_test.reshape(-1, 1)
-            sigma_pred = np.std(y_test - mu_test, axis=0)
-            print(f"residual test: {sigma_pred}")
+        print(f"residual train: {sigma_train}")
+        print(f"residual val: {sigma_val}")
 
         with catchtime("time to predict"):
             num_candidates = 10000
@@ -73,16 +102,7 @@ class TS(BaseSearcher):
             # simple for now
             if self.mu_pred.ndim == 1:
                 self.mu_pred = self.mu_pred.reshape(-1, 1)
-            self.sigma_pred = np.ones_like(self.mu_pred) * sigma_pred
-
-    def _subsample(self, X_train, z_train, max_samples: int = 10000):
-        assert len(X_train) == len(z_train)
-        X_train.reset_index(inplace=True)
-        if max_samples is not None and max_samples < len(X_train):
-            random_indices = np.random.permutation(len(X_train))[:max_samples]
-            X_train = X_train.loc[random_indices]
-            z_train = z_train[random_indices]
-        return X_train, z_train
+            self.sigma_pred = np.ones_like(self.mu_pred) * sigma_val
 
     def _update(self, trial_id: str, config: Dict, result: Dict):
         pass
@@ -104,11 +124,10 @@ class TS(BaseSearcher):
         }
 
 
-if __name__ == '__main__':
+def run_ts():
     bb, test_task = "nasbench201", "cifar100"
     bb, test_task = "fcnet", "protein_structure"
     bb_dict = load(bb)
-
 
     config_space = bb_dict[test_task].configuration_space
     metric_index = 0
@@ -122,7 +141,6 @@ if __name__ == '__main__':
         if task != test_task
     }
 
-
     blackbox = bb_dict[test_task]
     HyperbandScheduler(
         config_space=config_space,
@@ -131,40 +149,14 @@ if __name__ == '__main__':
             config_space=config_space,
             metric=bb_dict[test_task].objectives_names[metric_index],
             transfer_learning_evaluations=transfer_learning_evaluations,
+            max_fit_samples=5000,
         ),
         mode="min",
         metric=bb_dict[test_task].objectives_names[metric_index],
         max_t=200,
         resource_attr='hp_epoch',
     )
-#
-# if __name__ == '__main__':
-#
-#     bb_dict = load("nasbench201")
-#
-#     test_task = "cifar100"
-#     config_space = bb_dict[test_task].configuration_space
-#     metric_index = 0
-#     transfer_learning_evaluations = {
-#         task: TransferLearningTaskEvaluations(
-#             hyperparameters=bb.hyperparameters,
-#             # average over seed, take last fidelity and pick only first metric
-#             metrics=bb.objectives_evaluations.mean(axis=1)[:, -1, metric_index:metric_index+1]
-#         )
-#         for task, bb in bb_dict.items()
-#         if task != test_task
-#     }
-#
-#     bb_sch = BoundingBox(
-#         scheduler_fun=lambda new_config_space, mode, metric: FIFOScheduler(
-#             new_config_space,
-#             points_to_evaluate=[],
-#             searcher='random',
-#             metric=metric,
-#             mode=mode,
-#         ),
-#         mode="min",
-#         config_space=config_space,
-#         metric=bb_dict[test_task].objectives_names[metric_index],
-#         transfer_learning_evaluations=transfer_learning_evaluations,
-#     )
+
+
+if __name__ == '__main__':
+    run_ts()
