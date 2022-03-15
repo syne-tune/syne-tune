ee4e443dcfed5118286f4735f77477b8c56fab83
Ondrej Bohdal <ondrej.bohdal@gmail.com>
2022-03-15 15:12:55 +0000

    remove unused import

--- docs/tutorials/pasha/scripts/launch_pasha_nasbench201.py
+++ docs/tutorials/pasha/scripts/launch_pasha_nasbench201.py
@@ -9,7 +9,6 @@ from syne_tune.backend.simulator_backend.simulator_callback import SimulatorCall
 from benchmarking.definitions.definition_nasbench201 import nasbench201_benchmark, nasbench201_default_params
 from benchmarking.blackbox_repository.simulated_tabular_backend import BlackboxRepositoryBackend
 from benchmarking.blackbox_repository import load
-import random
 import pandas as pd
 import numpy as np
 import logging
