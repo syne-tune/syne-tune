115924c2e3c03f7adab04e74dbcba9307138664c
Matthias Seeger <matthis@amazon.de>
2022-04-06 16:02:38 +0200

    Fix flake

--- syne_tune/optimizer/schedulers/hyperband_promotion.py
+++ syne_tune/optimizer/schedulers/hyperband_promotion.py
@@ -10,7 +10,7 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
-from typing import List, Tuple, Optional
+from typing import List, Optional
 
 from syne_tune.optimizer.schedulers.hyperband_stopping import RungEntry, \
     quantile_cutoff, RungSystem
