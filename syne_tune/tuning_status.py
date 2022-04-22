4f6b414cc8deeb09f8dffb894d4d7d50e83bd723
wistuba <marwistu@amazon.com>
2022-04-22 13:42:20 +0200

    add import

--- syne_tune/tuning_status.py
+++ syne_tune/tuning_status.py
@@ -10,6 +10,7 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
+import numbers
 import time
 from collections import defaultdict, OrderedDict
 from typing import List, Dict, Tuple
