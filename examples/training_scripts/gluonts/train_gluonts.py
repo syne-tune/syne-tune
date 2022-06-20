9545d35c759f79e19ae0f2413605177524f55f7f
Lorenzo Stella <lorenzostella@gmail.com>
2022-06-20 15:56:44 +0200

    bump gluonts version in example (#269)

--- examples/training_scripts/gluonts/train_gluonts.py
+++ examples/training_scripts/gluonts/train_gluonts.py
@@ -10,9 +10,7 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
-"""
-Train a Cartpole with PPO using Ray, the discount factor can be tuned.
-"""
+
 import logging
 
 from gluonts.evaluation import make_evaluation_predictions, Evaluator
