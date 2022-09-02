299f6a264094bc7b59a9aa14caf525a37a8b9c51
aaronkl <kleinaa@cs.uni-freiburg.de>
2022-09-02 11:58:07 +0200

    add plateau stopper

--- syne_tune/stopping_criterion.py
+++ syne_tune/stopping_criterion.py
@@ -11,6 +11,8 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 import logging
+import numpy as np
+
 from dataclasses import dataclass
 from typing import Optional, Dict
 
@@ -44,32 +46,32 @@ class StoppingCriterion:
     # todo we should have unit-test for all those cases.
     def __call__(self, status: TuningStatus) -> bool:
         if (
-            self.max_wallclock_time is not None
-            and status.wallclock_time > self.max_wallclock_time
+                self.max_wallclock_time is not None
+                and status.wallclock_time > self.max_wallclock_time
         ):
             logger.info(
                 f"reaching max wallclock time ({self.max_wallclock_time}), stopping there."
             )
             return True
         if (
-            self.max_num_trials_started is not None
-            and status.num_trials_started > self.max_num_trials_started
+                self.max_num_trials_started is not None
+                and status.num_trials_started > self.max_num_trials_started
         ):
             logger.info(
                 f"reaching max number of trials started ({self.max_num_trials_started}), stopping there."
             )
             return True
         if (
-            self.max_num_trials_completed is not None
-            and status.num_trials_completed > self.max_num_trials_completed
+                self.max_num_trials_completed is not None
+                and status.num_trials_completed > self.max_num_trials_completed
         ):
             logger.info(
                 f"reaching max number of trials completed ({self.max_num_trials_completed}), stopping there."
             )
             return True
         if (
-            self.max_num_trials_finished is not None
-            and status.num_trials_finished > self.max_num_trials_finished
+                self.max_num_trials_finished is not None
+                and status.num_trials_finished > self.max_num_trials_finished
         ):
             logger.info(
                 f"reaching max number of trials finished ({self.max_num_trials_finished}), stopping there."
@@ -79,22 +81,22 @@ class StoppingCriterion:
             logger.info(f"reaching max cost ({self.max_cost}), stopping there.")
             return True
         if (
-            self.max_num_evaluations is not None
-            and status.overall_metric_statistics.count > self.max_num_evaluations
+                self.max_num_evaluations is not None
+                and status.overall_metric_statistics.count > self.max_num_evaluations
         ):
             logger.info(
                 f"reaching {status.overall_metric_statistics.count} evaluations, stopping there. "
             )
             return True
         if (
-            self.max_metric_value is not None
-            and status.overall_metric_statistics.count > 0
+                self.max_metric_value is not None
+                and status.overall_metric_statistics.count > 0
         ):
             max_metrics_observed = status.overall_metric_statistics.max_metrics
             for metric, max_metric_accepted in self.max_metric_value.items():
                 if (
-                    metric in max_metrics_observed
-                    and max_metrics_observed[metric] > max_metric_accepted
+                        metric in max_metrics_observed
+                        and max_metrics_observed[metric] > max_metric_accepted
                 ):
                     logger.info(
                         f"found {metric} with value ({max_metrics_observed[metric]}), "
@@ -103,14 +105,14 @@ class StoppingCriterion:
                     return True
 
         if (
-            self.min_metric_value is not None
-            and status.overall_metric_statistics.count > 0
+                self.min_metric_value is not None
+                and status.overall_metric_statistics.count > 0
         ):
             min_metrics_observed = status.overall_metric_statistics.min_metrics
             for metric, min_metric_accepted in self.min_metric_value.items():
                 if (
-                    metric in min_metrics_observed
-                    and min_metrics_observed[metric] < min_metric_accepted
+                        metric in min_metrics_observed
+                        and min_metrics_observed[metric] < min_metric_accepted
                 ):
                     logger.info(
                         f"found {metric} with value ({min_metrics_observed[metric]}), "
@@ -118,3 +120,89 @@ class StoppingCriterion:
                     )
                     return True
         return False
+
+
+class PlateauStopper(object):
+    """
+    Early stop the experiment when a metric plateaued across trials.
+    This code is mostly copied from RayTune.
+
+    Stops the entire experiment when the metric has plateaued
+    for more than the given amount of iterations specified in
+    the patience parameter.
+
+    :param metric: The metric to be monitored.
+    :param std: The minimal standard deviation after which
+             the tuning process has to stop.
+    :param top: The number of best models to consider.
+    :param mode: The mode to select the top results.
+             Can either be "min" or "max".
+    :param patience: Number of iterations to wait for
+             a change in the top models.
+    """
+
+    def __init__(
+            self,
+            metric: str,
+            std: float = 0.001,
+            top: int = 10,
+            mode: str = "min",
+            patience: int = 0,
+    ):
+        if mode not in ("min", "max"):
+            raise ValueError("The mode parameter can only be either min or max.")
+
+        if not isinstance(top, int) or top <= 1:
+            raise ValueError(
+                "Top results to consider must be"
+                " a positive integer greater than one."
+            )
+        if not isinstance(patience, int) or patience < 0:
+            raise ValueError("Patience must be a strictly positive integer.")
+        if not isinstance(std, float) or std <= 0:
+            raise ValueError(
+                "The standard deviation must be a strictly positive float number."
+            )
+        self._mode = mode
+        self._metric = metric
+        self._patience = patience
+        self._iterations = 0
+        self._std = std
+        self._top = top
+
+        if self._mode == 'min':
+            self.multiplier = 1
+        else:
+            self.multiplier = -1
+
+    def __call__(self, status: TuningStatus) -> bool:
+
+        """Return a boolean representing if the tuning has to stop."""
+
+        if status.num_trials_finished == 0:
+            return False
+
+        trials = status.trial_rows
+        trajectory = []
+        curr_best = None
+
+        for ti in trials.values():
+            if self._metric in ti:
+                y = self.multiplier * ti[self._metric]
+                if curr_best is None or y < curr_best:
+                    curr_best = y
+                trajectory.append(curr_best)
+
+        top_values = trajectory[-self._top:]
+        # If the current iteration has to stop
+        has_plateaued = len(top_values) == self._top and np.std(top_values) <= self._std
+        if has_plateaued:
+            # we increment the total counter of iterations
+            self._iterations += 1
+        else:
+            # otherwise we reset the counter
+            self._iterations = 0
+
+        # and then call the method that re-executes
+        # the checks, including the iterations.
+        return has_plateaued and self._iterations >= self._patience
---
+++ tst/test_plateau_stopper.py
@@ -0,0 +1,64 @@
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+import pytest
+import itertools
+
+from syne_tune.optimizer.baselines import RandomSearch
+from syne_tune import Tuner
+from syne_tune.stopping_criterion import PlateauStopper
+from syne_tune.config_space import randint, choice
+from syne_tune.util import script_height_example_path
+from tst.util_test import temporary_local_backend
+
+
+def test_plateau_scheduler():
+    max_steps = 5
+    num_workers = 1
+    random_seed = 382378624
+
+    config_space = {
+        "steps": max_steps,
+        "width": randint(0, 20),
+        "height": randint(-100, 100),
+        "sleep_time": 0.001,
+    }
+
+    entry_point = str(script_height_example_path())
+    metric = "mean_loss"
+    mode = 'min'
+
+    trial_backend = temporary_local_backend(entry_point=entry_point)
+
+    search_options = {"debug_log": False, "num_init_random": num_workers}
+
+    myscheduler = RandomSearch(
+        config_space,
+        search_options=search_options,
+        mode=mode,
+        metric=metric,
+        random_seed=random_seed,
+        points_to_evaluate=[{'width': 10, 'height': 0}, {'width': 7, 'height': 0}, {'width': 6, 'height': 0}]
+    )
+
+    stop_criterion = PlateauStopper(metric=metric, mode=mode, std=0.1, top=2, patience=3)
+    tuner = Tuner(
+        trial_backend=trial_backend,
+        scheduler=myscheduler,
+        sleep_time=0.1,
+        n_workers=num_workers,
+        stop_criterion=stop_criterion,
+    )
+
+    tuner.run()
+
+    assert tuner.tuning_status.num_trials_finished == 3
