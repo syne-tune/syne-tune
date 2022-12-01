76e967386f70c5c3122bac9bf49c5316553e175a
Matthias Seeger <matthis@amazon.de>
2022-12-01 11:08:02 +0100

    Small fixes

--- examples/training_scripts/checkpoint_example/checkpoint_example.py
+++ examples/training_scripts/checkpoint_example/checkpoint_example.py
@@ -18,6 +18,7 @@ import time
 from pathlib import Path
 
 from syne_tune import Reporter
+from syne_tune.constants import ST_CHECKPOINT_DIR
 
 
 report = Reporter()
@@ -44,16 +45,17 @@ if __name__ == "__main__":
     parser.add_argument("--sleep-time", type=float, default=0.1)
 
     # convention the path where to serialize and deserialize is given as st_checkpoint_dir
-    parser.add_argument("--st_checkpoint_dir", type=str)
+    parser.add_argument(f"--{ST_CHECKPOINT_DIR}", type=str)
 
     args, _ = parser.parse_known_args()
 
     num_epochs = args.num_epochs
     checkpoint_path = None
-    start_epoch = 0
+    start_epoch = 1
     current_value = 0
-    if args.st_checkpoint_dir is not None:
-        checkpoint_path = Path(args.st_checkpoint_dir) / "checkpoint.json"
+    checkpoint_dir = args.get(ST_CHECKPOINT_DIR)
+    if checkpoint_dir is not None:
+        checkpoint_path = Path(checkpoint_dir) / "checkpoint.json"
         if checkpoint_path.exists():
             state = load_checkpoint(checkpoint_path)
             logging.info(f"resuming from previous checkpoint {state}")
@@ -62,9 +64,9 @@ if __name__ == "__main__":
 
     # write dumb values for loss to illustrate sagemaker ability to retrieve metrics
     # should be replaced by your algorithm
-    for current_epoch in range(start_epoch, num_epochs):
+    for current_epoch in range(start_epoch, num_epochs + 1):
+        time.sleep(args.sleep_time)
         current_value = (current_value + 1) * args.multiplier
-        report(train_acc=current_value, step=current_epoch)
         if checkpoint_path is not None:
             save_checkpoint(checkpoint_path, current_epoch, current_value)
-        time.sleep(args.sleep_time)
+        report(train_acc=current_value, epoch=current_epoch)
