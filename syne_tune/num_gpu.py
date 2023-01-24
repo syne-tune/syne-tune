6122820ff1e255c8903c3678c7966d91a7f9e120
David Salinas <dsalina@amazon.com>
2023-01-24 09:51:45 +0100

    fix number of gpu computation

--- syne_tune/num_gpu.py
+++ syne_tune/num_gpu.py
@@ -42,9 +42,12 @@ def get_num_gpus() -> int:
             if proc.poll() is None:
                 raise ValueError("nvidia-smi timed out after 10 secs.")
 
-            with open("std.out", "r") as stdout:
-                _num_gpus = len(stdout.readlines())
-            return _num_gpus
+            if proc.poll() != 0:
+                return 0
+            else:
+                with open("std.out", "r") as stdout:
+                    _num_gpus = len(stdout.readlines())
+                return _num_gpus
 
         except (OSError, FileNotFoundError):
             logging.info(
