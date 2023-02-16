106d76eebbda51dcaaaa80737c7c16c234e8bb01
Jacek Golebiowski <jacekgo@amazon.com>
2023-02-16 11:32:10 +0100

    Examples should now use appropriate remote launchers

--- examples/launch_custom_benchmark_remotely.py
+++ examples/launch_custom_benchmark_remotely.py
@@ -13,11 +13,16 @@
 """
 This example show how to launch a tuning job that will orchestrated on Sagemaker rather than on your local machine.
 """
-
+import argparse
 import logging
 from pathlib import Path
 
-from benchmarking.commons.launch_remote_sagemaker import launch_remote
+from benchmarking.commons.launch_remote_sagemaker import (
+    launch_remote as launch_remote_sagemaker,
+)
+from benchmarking.commons.launch_remote_local import (
+    launch_remote as launch_remote_local,
+)
 from benchmarking.nursery.launch_sagemaker.baselines import (
     methods as all_methods,
     Methods,
@@ -30,13 +35,38 @@ if __name__ == "__main__":
     The benchmark name needs to be passed using --benchmark <bench_name> command line argument.
     For the custom height example, one needs to specify `--benchmark height_benchmark`
     """
+    parser = argparse.ArgumentParser(
+        description="Launch a custom <train_height.py> benchmark using selected backend"
+    )
+    parser.add_argument(
+        "--backend",
+        choices=["local", "sagemaker"],
+        required=True,
+        type=str,
+        help="Backed to use for experiment",
+    )
+    parser.add_argument(
+        "--benchmark",
+        type=str,
+        required=True,
+        help="Benchmark to run, should be <height_benchmark> for this example",
+    )
+    args, unknown = parser.parse_known_args()
+
     logging.getLogger().setLevel(logging.INFO)
     entry_point = Path(__file__).parent / "launch_custom_benchmark.py"
     single_fidelity_methods = {
         Methods.RS: all_methods[Methods.RS],
         Methods.BO: all_methods[Methods.BO],
     }
-    launch_remote(
+
+    print(f"Got the backend argument of {args.backend}, starting tuning")
+    if args.backend == "local":
+        launcher = launch_remote_local
+    elif args.backend == "sagemaker":
+        launcher = launch_remote_sagemaker
+
+    launcher(
         entry_point=entry_point,
         methods=single_fidelity_methods,
         benchmark_definitions=height_benchmark,
