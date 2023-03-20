c328d3adc65fe1c5c164dd082ab1af9b5f93a985
aaronkl <kleinaa@cs.uni-freiburg.de>
2023-03-20 09:44:51 +0100

    add stopping criterion

---
+++ examples/example_automatic_termination_criterion.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+
+from syne_tune.stopping_criterions.automatic_termination_criterion import AutomaticTerminationCriterion
+from syne_tune.blackbox_repository import BlackboxRepositoryBackend
+from syne_tune.backend.simulator_backend.simulator_callback import SimulatorCallback
+from syne_tune.optimizer.baselines import BORE
+from syne_tune import Tuner, StoppingCriterion
+from syne_tune.experiments import load_experiment
+
+from benchmarking.commons.benchmark_definitions.nas201 import nas201_benchmark
+
+
+n_workers = 4
+dataset_name = "cifar10"
+benchmark = nas201_benchmark(dataset_name)
+max_wallclock_time = 3600 * 6
+num_seeds = 10
+
+criterions = ['time', 'auto']
+fig, axis = plt.subplots(1, 2, dpi=200, figsize=(12, 6), sharex=True, sharey=True)
+
+for i, criterion in enumerate(criterions):
+    axis[i].set_title(criterion)
+
+    for seed in range(num_seeds):
+
+        if criterion == 'time':
+            stop_criterion = StoppingCriterion(max_wallclock_time=max_wallclock_time)
+
+        else:
+            stop_criterion = AutomaticTerminationCriterion(blackbox.configuration_space, threshold=0.1,
+                                                           metric=benchmark.metric, mode=benchmark.mode,
+                                                           seed=seed,
+                                                           )
+        trial_backend = BlackboxRepositoryBackend(
+            elapsed_time_attr=benchmark.elapsed_time_attr,
+            blackbox_name=benchmark.blackbox_name,
+            dataset=dataset_name,
+            seed=seed % 3
+        )
+        blackbox = trial_backend.blackbox
+
+        scheduler = BORE(
+            config_space=blackbox.configuration_space,
+            mode=benchmark.mode,
+            metric=benchmark.metric,
+            search_options={"debug_log": False},
+            random_seed=seed,
+        )
+
+        print_update_interval = 700
+        results_update_interval = 300
+        tuner = Tuner(
+            trial_backend=trial_backend,
+            scheduler=scheduler,
+            stop_criterion=stop_criterion,
+            n_workers=n_workers,
+            sleep_time=0,
+            results_update_interval=results_update_interval,
+            print_update_interval=print_update_interval,
+            callbacks=[SimulatorCallback()],
+        )
+        tuner.run()
+
+        exp = load_experiment(tuner.name)
+        traj = list(exp.results[benchmark.metric].cummin())
+        runtime = list(exp.results['st_tuner_time'])
+
+        axis[i].plot(runtime, traj, color=f'C{i}')
+        axis[i].plot(runtime[-1], traj[-1], color=f'C{i}', marker='o')
+
+        axis[i].set_ylim(0.05, 0.3)
+        axis[i].set_xlim(0, max_wallclock_time)
+fig.add_subplot(111, frameon=False)
+plt.tick_params(labelcolor='none', which='both', top=False, bottom=False, left=False, right=False)
+plt.ylabel('validation error')
+plt.xlabel('wall-clock time (seconds)')
+plt.savefig('/Users/kleiaaro/auto.png')
+plt.show()
---
+++ syne_tune/stopping_criterions/automatic_termination_criterion.py
@@ -0,0 +1,193 @@
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+import logging
+import numpy as np
+import torch
+from typing import List, Dict, Any
+
+from syne_tune.tuning_status import TuningStatus
+from syne_tune.optimizer.schedulers.searchers.utils import (
+    make_hyperparameter_ranges,
+)
+from torch import Tensor
+from botorch.models import SingleTaskGP
+from botorch.fit import fit_gpytorch_mll
+from botorch.utils.transforms import normalize
+from botorch.acquisition import UpperConfidenceBound
+from botorch.optim import optimize_acqf
+from gpytorch.mlls import ExactMarginalLogLikelihood
+
+logger = logging.getLogger(__name__)
+
+EPSILON_LOG = 1e-10
+NOISE_LEVEL = 1e-3
+
+
+class AutomaticTerminationCriterion(object):
+    """
+    Implements the automatic termination criterion proposed by Makarova et al. The idea is to automatically
+    stop the optimization process if the expected gain in regret is smaller than the observation noise
+    of the objective.
+
+    Automatic Termination for Hyperparameter Optimization
+    Anastasia Makarova and Huibin Shen and Valerio Perrone and Aaron Klein and Jean Baptiste Faddoul and Andreas Krause and Matthias Seeger and Cedric Archambeau
+    First Conference on Automated Machine Learning (Main Track)
+    2022
+
+    :param config_space: Configuration space for evaluation function
+    :param metric: The metric to be monitored.
+    :param mode: The mode to select the top results ("min" or "max")
+    :param threshold: The threshold on the regret. We stop the optimization process when it's unlikely that the expected decrease in regret compared to the global optimum is higher than this threshold.
+    :param beta: Multiplier on the standard devication to compute the upper and lower confidence bound.
+    :param seed: Seed for the random numer generator.
+    :param warm_up: Number of iterations before we start fitting the GP. Especially, with little data, GP's are notoriously unreliable. Hence we only fit the GP once we collected some data points
+    :param topq: We consider only the top q-% data points to fit the GP.
+    """
+
+    def __init__(
+            self,
+            config_space: Dict[str, Any],
+            metric: str,
+            threshold: float,
+            mode: str = 'min',
+            beta: float = 1.0,
+            seed: int = None,
+            warm_up: int = 10,
+            topq: float = 0.5,
+    ):
+        assert mode in ["min", "max"]
+
+        self._mode = mode
+        self._warm_up = warm_up
+        self._seed = np.random.randint(2 ** 32 - 1) if seed is None else seed
+        self._metric = metric
+        self._topq = topq
+        self._beta = beta
+        self._threshold = threshold
+        self._config_space = config_space
+        self._hp_ranges = make_hyperparameter_ranges(config_space)
+
+        if self._mode == "max":
+            self.multiplier = 1
+        else:
+            self.multiplier = -1
+
+    def _config_to_feature_matrix(self, configs: List[dict]) -> Tensor:
+        bounds = Tensor(self._hp_ranges.get_ndarray_bounds()).T
+        X = Tensor(self._hp_ranges.to_ndarray_matrix(configs))
+        return normalize(X, bounds)
+
+    def __call__(self, status: TuningStatus) -> bool:
+        """Return a boolean representing if the tuning has to stop."""
+
+        trials = status.trial_rows
+
+        if len(trials) == 0:
+            return False
+
+        evaluations = []
+        observations = []
+        for trial in trials.values():
+            if self._metric not in trial:
+                continue
+            y = self.multiplier * trial[self._metric]
+            evaluations.append(y)
+
+            observation = {}
+            for hp in self._config_space:
+                observation[hp] = trial[hp]
+            observations.append(observation)
+
+        if len(observations) < self._warm_up:
+            return False
+
+        observations = self._config_to_feature_matrix(observations).double()
+        evaluations = torch.tensor(evaluations).double()
+
+        noise_std = NOISE_LEVEL
+        evaluations += noise_std * torch.randn_like(evaluations)
+
+        n = len(observations)
+
+        if self._topq is not None:
+            topn = max(20, int(n * self._topq))
+            top_inds = torch.argsort(evaluations, descending=True, dim=0)[:topn]
+            evaluations = evaluations[top_inds]
+            observations = observations[top_inds]
+
+        if len(observations.shape) == 1:
+            observations = observations.reshape((-1, 1))
+        if len(evaluations.shape) == 1:
+            evaluations = evaluations.reshape((-1, 1))
+
+        gp = SingleTaskGP(observations, evaluations)
+        mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
+        fit_gpytorch_mll(mll, max_attempts=1)
+        acq = UpperConfidenceBound(
+            model=gp,
+            beta=self._beta,
+        )
+        _, max_ucb = optimize_acqf(
+            acq,
+            bounds=Tensor(self._hp_ranges.get_ndarray_bounds()).T,
+            q=1,
+            num_restarts=3,
+            raw_samples=100,
+        )
+        posterior = gp.posterior(observations)
+        mean = posterior.mean
+        std = torch.sqrt(posterior.variance)
+        lcb = mean - self._beta * std
+        max_lcb = lcb.max()
+
+        upper_bound = max_ucb - max_lcb
+
+        stop = upper_bound < self._threshold
+
+        return stop
+
+
+if __name__ == "__main__":
+
+
+    from syne_tune.backend.trial_status import Trial, Status
+    from syne_tune.config_space import uniform
+
+    metric = "loss"
+    config_space = {"x": uniform(0, 1)}
+
+    stop_criterion = AutomaticTerminationCriterion(config_space, threshold=0.9,
+                                                   metric=metric, mode="min"
+                                                   )
+
+    status = TuningStatus(metric_names=[metric])
+
+    trial_status_dict = {}
+    new_results = []
+    for i in range(20):
+        x = np.random.rand()
+        trial = Trial(trial_id=i, config={"x": x}, creation_time=None)
+        trial_status_dict[i] = (trial, Status.completed)
+        new_results.append((i, {metric: (x-0.5)**2}))
+    status.update(trial_status_dict, new_results)
+    # status.update(
+    #     trial_status_dict={
+    #         0: (trial0, Status.completed),
+    #         1: (trial1, Status.completed),
+    #     },
+    #     new_results=[
+    #         (0, {metric: 4.0}),
+    #         (1, {metric: 3.0}),
+    #     ],
+    # )
+    assert not stop_criterion(status)
