a0b8178f6268f38530210d396a6288b306c77a3a
Jacek Golebiowski <jacekgo@amazon.com>
2023-03-30 16:10:16 +0200

    Added the two plotting functions to analyse multi-objective experiments in Syne Tune.

--- syne_tune/optimizer/schedulers/multiobjective/utils.py
+++ syne_tune/optimizer/schedulers/multiobjective/utils.py
@@ -1,3 +1,15 @@
+# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
 import numpy as np
 
 from syne_tune.try_import import try_import_moo_message
@@ -10,7 +22,8 @@ except ImportError:
 
 EPSILON = 1e-6
 
-def hypervolume(results_array: np.ndarray, reference_points: np.ndarray= None):
+
+def hypervolume(results_array: np.ndarray, reference_points: np.ndarray = None):
     """
     Compute the hypervolume of all results based on reference points
 
@@ -26,6 +39,6 @@ def hypervolume(results_array: np.ndarray, reference_points: np.ndarray= None):
     hypervolume_indicator = np.zeros(shape=len(results_array))
 
     for idx in range(len(results_array)):
-        hypervolume_indicator[idx] = indicator_fn(results_array[0: idx])
+        hypervolume_indicator[idx] = indicator_fn(results_array[0:idx])
 
     return hypervolume_indicator
