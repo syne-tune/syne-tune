80993dbc58125d10520003ad9bd2427116fac952
mseeger <matthis@amazon.de>
2023-05-11 16:08:19 +0200

    fix: Fix import bug, and also new workflow to install Syne Tune with … (#663)

--- syne_tune/optimizer/schedulers/multiobjective/nsga2_searcher.py
+++ syne_tune/optimizer/schedulers/multiobjective/nsga2_searcher.py
@@ -36,26 +36,35 @@ except ImportError:
     print(try_import_moo_message())
 
 
-class MultiObjectiveMixedVariableProblem(Problem):
-    def __init__(self, n_obj: int, config_space: Dict[str, Any], **kwargs):
-        vars = {}
+def _create_multiobjective_problem(config_space: Dict[str, Any], n_obj: int, **kwargs):
+    # This needs to be an inner class, since ``Problem`` can only be imported
+    # with ``moo`` dependencies. We want this module to be importable even if
+    # ``moo`` dependencies are not present: only creating a ``NSGA2Searcher``
+    # object should fail in this case.
+    class _MultiObjectiveMixedVariableProblem(Problem):
+        def __init__(self, n_obj: int, config_space: Dict[str, Any], **kwargs):
+            vars = {}
 
-        for hp_name, hp in config_space.items():
-            if isinstance(hp, Domain):
-                if isinstance(hp, Categorical):
-                    vars[hp_name] = PyMOOChoice(options=hp.categories)
-                elif isinstance(hp, Integer):
-                    vars[hp_name] = PyMOOInteger(bounds=(hp.lower, hp.upper - 1))
-                elif isinstance(hp, FiniteRange):
-                    vars[hp_name] = PyMOOInteger(bounds=(0, hp.size - 1))
-                elif isinstance(hp, Float):
-                    vars[hp_name] = PyMOOReal(bounds=(hp.lower, hp.upper))
-                else:
-                    raise Exception(
-                        f"Type {type(hp)} of hyperparameter {hp_name} is not supported!"
-                    )
+            for hp_name, hp in config_space.items():
+                if isinstance(hp, Domain):
+                    if isinstance(hp, Categorical):
+                        vars[hp_name] = PyMOOChoice(options=hp.categories)
+                    elif isinstance(hp, Integer):
+                        vars[hp_name] = PyMOOInteger(bounds=(hp.lower, hp.upper - 1))
+                    elif isinstance(hp, FiniteRange):
+                        vars[hp_name] = PyMOOInteger(bounds=(0, hp.size - 1))
+                    elif isinstance(hp, Float):
+                        vars[hp_name] = PyMOOReal(bounds=(hp.lower, hp.upper))
+                    else:
+                        raise Exception(
+                            f"Type {type(hp)} of hyperparameter {hp_name} is not supported!"
+                        )
 
-        super().__init__(vars=vars, n_obj=n_obj, n_ieq_constr=0, **kwargs)
+            super().__init__(vars=vars, n_obj=n_obj, n_ieq_constr=0, **kwargs)
+
+    return _MultiObjectiveMixedVariableProblem(
+        n_obj=n_obj, config_space=config_space, **kwargs
+    )
 
 
 class NSGA2Searcher(StochasticSearcher):
@@ -115,8 +124,8 @@ class NSGA2Searcher(StochasticSearcher):
                     FiniteRange,
                 ], f"Type {type(hp)} for hyperparameter {hp_name} is not supported."
 
-        self.problem = MultiObjectiveMixedVariableProblem(
-            config_space=config_space, n_var=len(self.hp_names), n_obj=len(metric)
+        self.problem = _create_multiobjective_problem(
+            config_space=config_space, n_obj=len(metric), n_var=len(self.hp_names)
         )
         self.algorithm = NSGA2(
             pop_size=population_size,
