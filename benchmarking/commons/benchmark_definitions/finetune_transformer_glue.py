fcf49e23059e00c43a3ca82a70cb3e40b15b8cb5
Matthias Seeger <matthis@amazon.de>
2023-05-25 23:56:48 +0200

    Fix lint

--- benchmarking/commons/benchmark_definitions/finetune_transformer_glue.py
+++ benchmarking/commons/benchmark_definitions/finetune_transformer_glue.py
@@ -10,7 +10,7 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
-from typing import Dict, Optional
+from typing import Dict
 from pathlib import Path
 
 from benchmarking.commons.benchmark_definitions.common import RealBenchmarkDefinition
