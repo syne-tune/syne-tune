054b2126e554de16f76ebae5e05dbc833ac127f6
Matthias Seeger <matthis@amazon.de>
2023-06-09 09:46:30 +0200

    More comments

--- syne_tune/experiments/multiobjective.py
+++ syne_tune/experiments/multiobjective.py
@@ -27,7 +27,30 @@ def hypervolume_indicator_column_generator(
     Returns generator for new dataframe column containing the best hypervolume
     indicator as function of wall-clock time, based on the metrics in
     ``metrics_and_modes`` (metric names correspond to column names in the
-    dataframe). For a metric with ``mode == "min"``, we use its negative.
+    dataframe). For a metric with ``mode == "max"``, we use its negative.
+
+    This mapping is used to create the ``dataframe_column_generator`` argument
+    of :meth:`~syne_tune.experiments.ComparativeResults.plot`. Since the
+    current implementation is not incremental and quite slow, if you plot
+    results for single-fidelity HPO methods, it is strongly recommended to
+    also use ``one_result_per_trial=True``:
+
+    .. code:: python
+
+       results = ComparativeResults(...)
+       dataframe_column_generator = hypervolume_indicator_column_generator(
+           metrics_and_modes
+       )
+       plot_params = PlotParameters(
+           metric="hypervolume_indicator",
+           mode="max",
+       )
+       results.plot(
+           benchmark_name=benchmark_name,
+           plot_params=plot_params,
+           dataframe_column_generator=dataframe_column_generator,
+           one_result_per_trial=True,
+       )
 
     :param metrics_and_modes: List of ``(metric, mode)``, see above
     :param reference_point: Reference point for hypervolume computation. If not
