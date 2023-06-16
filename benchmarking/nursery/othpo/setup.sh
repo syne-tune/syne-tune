f1a64f8ea8b6261b78b38a456e0af54452ec084a
Sigrid Passano Hellan <sighellan@gmail.com>
2023-06-16 18:55:21 +0100

    Remove xgboost result files, use wget to collect json from fork.

--- benchmarking/nursery/othpo/setup.sh
+++ benchmarking/nursery/othpo/setup.sh
@@ -6,3 +6,4 @@ python simopt/generate_simopt_fixed_factors.py
 
 echo "Generating folder for XGBoost results"
 mkdir -p xgboost_experiment_results/random-mnist
+wget -P xgboost_experiment_results/random-mnist https://raw.githubusercontent.com/sighellan/syne-tune/othpo-results/benchmarking/nursery/othpo/xgboost_experiment_results/random-mnist/aggregated_experiments.json
