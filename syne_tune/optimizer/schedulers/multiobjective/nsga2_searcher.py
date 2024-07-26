ef81bcce9d4f50b57c2ad1f18bcc03d3e9e9563d
Aaron <kleiaaro@gmail.com>
2024-07-26 12:56:26 +0200

    format

--- syne_tune/optimizer/schedulers/multiobjective/nsga2_searcher.py
+++ syne_tune/optimizer/schedulers/multiobjective/nsga2_searcher.py
@@ -11,7 +11,6 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 import logging
-
 import numpy as np
 
 from typing import Optional, List, Union, Dict, Any
