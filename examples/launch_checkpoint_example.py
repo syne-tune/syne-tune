30b8925b4e30ca9e2362f21f3c30842696646c0c
kavlahkaff <de28oser@studserv.uni-leipzig.de>
2024-10-17 16:25:56 +0200

    refactor: removed amazon license

--- examples/launch_checkpoint_example.py
+++ examples/launch_checkpoint_example.py
@@ -1,15 +1,3 @@
-# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License is located at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# or in the "license" file accompanying this file. This file is distributed
-# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-# express or implied. See the License for the specific language governing
-# permissions and limitations under the License.
 """
 An example showing how to retrieve the best checkpoint of an XGBoost model.
 The script being tuned ``xgboost_checkpoint.py`` stores the checkpoint obtained after each trial evaluation.
