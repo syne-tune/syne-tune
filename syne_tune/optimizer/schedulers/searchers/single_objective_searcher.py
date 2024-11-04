491074be74aec9c5a3c6c94b71f2695e7bc50514
Aaron <kleiaaro@gmail.com>
2024-11-04 12:24:35 +0100

    remove unused imports

--- syne_tune/optimizer/schedulers/searchers/single_objective_searcher.py
+++ syne_tune/optimizer/schedulers/searchers/single_objective_searcher.py
@@ -1,7 +1,6 @@
 import logging
-import numpy as np
 
-from typing import Optional, List, Dict, Any
+from typing import Dict, Any
 
 from syne_tune.optimizer.schedulers.searchers.searcher import BaseSearcher
 
