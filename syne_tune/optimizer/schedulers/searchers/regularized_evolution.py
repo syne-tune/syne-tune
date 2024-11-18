28b402e291755f8988ed1282adf030e434f280be
Aaron <kleiaaro@gmail.com>
2024-11-18 11:04:11 +0100

    update to new interface

--- syne_tune/optimizer/schedulers/searchers/regularized_evolution.py
+++ syne_tune/optimizer/schedulers/searchers/regularized_evolution.py
@@ -1,28 +1,52 @@
 import copy
 import logging
 
+import numpy as np
+
 from collections import deque
-from typing import Optional, List, Dict, Any, Union
+from typing import Optional, List, Dict, Any
 from dataclasses import dataclass
 
-from syne_tune.optimizer.schedulers.searchers import StochasticSearcher
-from syne_tune.config_space import config_space_size, Domain
-from syne_tune.optimizer.schedulers.searchers.utils import make_hyperparameter_ranges
-from syne_tune.optimizer.schedulers.searchers.searcher_base import (
-    sample_random_configuration,
-)
+from syne_tune.config_space import config_space_size, non_constant_hyperparameter_keys
+from syne_tune.optimizer.schedulers.searchers.single_objective_searcher import SingleObjectiveBaseSearcher
+
 
 logger = logging.getLogger(__name__)
 
 
 @dataclass
 class PopulationElement:
-    result: Dict[str, Any] = None
-    score: int = 0
+    score: float = 0
     config: Dict[str, Any] = None
+    results: list[float] = None
 
 
-class RegularizedEvolution(StochasticSearcher):
+def mutate_config(config: Dict[str, Any],
+                  config_space: Dict[str, Any],
+                  rng: np.random.RandomState,
+                  num_try: int = 1000) -> Dict[str, Any]:
+
+    child_config = copy.deepcopy(config)
+    hp_name = rng.choice(non_constant_hyperparameter_keys(config_space))
+
+    for i in range(num_try):
+        child_config[hp_name] = config_space[hp_name].sample(random_state=rng)
+
+        # make sure that we actually sample a new value
+        if child_config[hp_name] != config[hp_name]:
+            break
+
+    return child_config
+
+
+def sample_random_config(config_space: Dict[str, Any], rng: np.random.RandomState) -> Dict[str, Any]:
+    return {
+        k: v.sample() if hasattr(v, "sample") else v
+        for k, v in config_space.items()
+    }
+
+
+class RegularizedEvolution(SingleObjectiveBaseSearcher):
     """
     Implements the regularized evolution algorithm. The original implementation
     only considers categorical hyperparameters. For integer and float parameters
@@ -39,8 +63,6 @@ class RegularizedEvolution(StochasticSearcher):
     Additional arguments on top of parent class
     :class:`~syne_tune.optimizer.schedulers.searchers.StochasticSearcher`:
 
-    :param mode: Mode to use for the metric given, can be "min" or "max",
-        defaults to "min"
     :param population_size: Size of the population, defaults to 100
     :param sample_size: Size of the candidate set to obtain a parent for the
         mutation, defaults to 10
@@ -49,14 +71,15 @@ class RegularizedEvolution(StochasticSearcher):
     def __init__(
         self,
         config_space,
-        metric: Union[List[str], str],
         points_to_evaluate: Optional[List[dict]] = None,
         population_size: int = 100,
         sample_size: int = 10,
-        **kwargs,
+        random_seed: int = None,
     ):
         super(RegularizedEvolution, self).__init__(
-            config_space, metric, points_to_evaluate=points_to_evaluate, **kwargs
+            config_space=config_space,
+            random_seed=random_seed,
+            points_to_evaluate=points_to_evaluate,
         )
         assert (
             config_space_size(config_space) != 1
@@ -64,89 +87,38 @@ class RegularizedEvolution(StochasticSearcher):
         self.population_size = population_size
         self.sample_size = sample_size
         self.population = deque()
-        self.num_sample_try = 1000  # number of times allowed to sample a mutation
-        self._hp_ranges = make_hyperparameter_ranges(self.config_space)
-        allow_duplicates = kwargs.get("allow_duplicates")
-        if allow_duplicates is not None and (not allow_duplicates):
-            logger.warning(
-                "This class does not support allow_duplicates argument. Sampling is with replacement"
-            )
-        if kwargs.get("restrict_configurations") is not None:
-            logger.warning("This class does not support restrict_configurations")
-        self._non_constant_hps = [
-            name
-            for name, domain in config_space.items()
-            if isinstance(domain, Domain) and len(domain) != 1
-        ]
+        self.random_state = np.random.RandomState(self.random_seed)
 
-    def _mutate_config(self, config: Dict[str, Any]) -> Dict[str, Any]:
-        child_config = copy.deepcopy(config)
-        config_ms = self._hp_ranges.config_to_match_string(config)
-        # Sample mutation until a different configuration is found
-        config_is_mutated = False
-        for _ in range(self.num_sample_try):
-            if self._hp_ranges.config_to_match_string(child_config) == config_ms:
-                # Sample a random hyperparameter to mutate
-                hp_name = self.random_state.choice(self._non_constant_hps, 1).item()
-                # Mutate the value by sampling
-                child_config[hp_name] = self.config_space[hp_name].sample(
-                    random_state=self.random_state
-                )
-            else:
-                config_is_mutated = True
-                break
-        if not config_is_mutated:
-            logger.info(
-                "Did not manage to sample a different configuration with "
-                f"{self.num_sample_try}, sampling at random"
-            )
-            child_config = self._sample_random_config()
 
-        return child_config
-
-    def _sample_random_config(self) -> Dict[str, Any]:
-        return sample_random_configuration(self._hp_ranges, self.random_state)
-
-    def get_config(self, **kwargs) -> Optional[dict]:
-        initial_config = self._next_initial_config()
+    def suggest(self, **kwargs) -> Optional[dict]:
+        initial_config = self._next_points_to_evaluate()
         if initial_config is not None:
             return initial_config
 
         if len(self.population) < self.population_size:
-            config = self._sample_random_config()
+            config = sample_random_config(config_space=self.config_space, rng=self.random_state)
         else:
             candidates = self.random_state.choice(
                 list(self.population), size=self.sample_size
             )
             parent = min(candidates, key=lambda i: i.score)
 
-            config = self._mutate_config(parent.config)
+            config = mutate_config(parent.config,
+                                   config_space=self.config_space,
+                                   rng=self.random_state)
 
         return config
 
-    def _update(self, trial_id: str, config: Dict[str, Any], result: Dict[str, Any]):
-        score = result[self._metric]
-
-        if self._mode == "max":
-            score *= -1
-
+    def on_trial_result(
+            self,
+            trial_id: int,
+            config: Dict[str, Any],
+            metric: float,
+    ):
         # Add element to the population
-        element = PopulationElement(result=result, score=score, config=config)
+        element = PopulationElement(score=metric, config=config)
         self.population.append(element)
 
         # Remove the oldest element of the population.
         if len(self.population) > self.population_size:
             self.population.popleft()
-
-    def configure_scheduler(self, scheduler):
-        from syne_tune.optimizer.schedulers.scheduler_searcher import (
-            TrialSchedulerWithSearcher,
-        )
-
-        assert isinstance(
-            scheduler, TrialSchedulerWithSearcher
-        ), "This searcher requires TrialSchedulerWithSearcher scheduler"
-        super().configure_scheduler(scheduler)
-
-    def clone_from_state(self, state: Dict[str, Any]):
-        raise NotImplementedError
--- tst/schedulers/test_rea.py
+++ tst/schedulers/test_rea.py
@@ -1,9 +1,12 @@
 import pytest
 
+import numpy as np
+
 from syne_tune.config_space import randint, choice
 from syne_tune.optimizer.schedulers.searchers.regularized_evolution import (
-    RegularizedEvolution,
+    RegularizedEvolution, mutate_config, sample_random_config
 )
+from syne_tune.optimizer.schedulers.multiobjective.multi_objective_regularized_evolution import MultiObjectiveRegularizedEvolution
 
 
 def test_rea_config_space_size_one():
@@ -16,7 +19,88 @@ def test_rea_config_space_size_one():
     with pytest.raises(AssertionError):
         searcher = RegularizedEvolution(
             config_space=config_space,
-            metric="error",
-            mode="min",
             random_seed=314159,
         )
+
+def test_random_config():
+    config_space = {
+        "a": randint(lower=1, upper=5),
+        "b": 25
+    }
+
+    config = sample_random_config(config_space, rng=np.random.RandomState(42))
+    assert "a" in config
+    assert 1 <= config['a'] <= 5
+    assert "b" in config
+    assert config['b'] == 25
+
+
+def test_mutate_config():
+    config_space = {
+        "a": randint(lower=1, upper=5),
+        "b": 25
+    }
+
+    config = {"a": 1, "b": 25}
+    mutated_config = mutate_config(config, config_space, rng=np.random.RandomState(42))
+
+    assert config['b'] == mutated_config['b']
+    assert config['a'] != mutated_config['a']
+
+
+def test_rea_population():
+
+    config_space = {
+        "a": randint(lower=1, upper=100),
+        "b": choice(["a", "b", "c", "d"]),
+    }
+
+    pop_size = 5
+
+    searcher = RegularizedEvolution(
+        config_space=config_space,
+        random_seed=314159,
+        population_size=pop_size
+    )
+
+    history = []
+    for i in range(pop_size):
+        config = searcher.suggest()
+        searcher.on_trial_result(i, config, np.random.rand())
+        history.append(config)
+
+    assert len(searcher.population) == 5
+    config = searcher.suggest()
+    searcher.on_trial_result(pop_size + 1, config, np.random.rand())
+
+    # assert that we removed the oldest element from the population
+    assert history[0] not in searcher.population
+
+
+def test_mo_rea_population():
+
+    config_space = {
+        "a": randint(lower=1, upper=100),
+        "b": choice(["a", "b", "c", "d"]),
+    }
+
+    pop_size = 5
+
+    searcher = MultiObjectiveRegularizedEvolution(
+        config_space=config_space,
+        random_seed=314159,
+        population_size=pop_size
+    )
+
+    history = []
+    for i in range(pop_size):
+        config = searcher.suggest()
+        searcher.on_trial_result(i, config, [np.random.rand(), np.random.rand()])
+        history.append(config)
+
+    assert len(searcher.population) == 5
+    config = searcher.suggest()
+    searcher.on_trial_result(pop_size + 1, config, [np.random.rand(), np.random.rand()])
+
+    # assert that we removed the oldest element from the population
+    assert history[0] not in searcher.population
