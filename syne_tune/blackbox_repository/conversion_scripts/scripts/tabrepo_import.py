72e2a6e51b2f5d49c6e015785c30f1a3aad4a2d2
kavlahkaff <de28oser@studserv.uni-leipzig.de>
2024-12-19 15:55:41 +0100

    format

--- syne_tune/blackbox_repository/conversion_scripts/scripts/tabrepo_import.py
+++ syne_tune/blackbox_repository/conversion_scripts/scripts/tabrepo_import.py
@@ -262,7 +262,7 @@ if __name__ == "__main__":
         TabrepoCatBoost,
         TabrepoXGBoost,
         TabrepoLightGBM,
-        #TabrepoLinearModel
+        # TabrepoLinearModel
     ]
 
     for recipe in recipes:
