589115b464365cefb393ec367111290f25168607
Aaron <kleiaaro@gmail.com>
2025-02-06 13:48:30 +0100

    format

--- tst/schedulers/transfer_learning/test_bounding_box.py
+++ tst/schedulers/transfer_learning/test_bounding_box.py
@@ -2,9 +2,13 @@ import numpy as np
 import pandas as pd
 
 from syne_tune.optimizer.schedulers.transfer_learning.bounding_box import BoundingBox
-from syne_tune.optimizer.schedulers.transfer_learning import TransferLearningTaskEvaluations
+from syne_tune.optimizer.schedulers.transfer_learning import (
+    TransferLearningTaskEvaluations,
+)
 from syne_tune.config_space import randint, uniform, choice
-from syne_tune.optimizer.schedulers.single_objective_scheduler import SingleObjectiveScheduler
+from syne_tune.optimizer.schedulers.single_objective_scheduler import (
+    SingleObjectiveScheduler,
+)
 
 
 config_space = {
@@ -16,45 +20,58 @@ config_space = {
 metric = "objective1"
 random_seed = 42
 
+
 def create_task_evaluation():
     num_evals = 100
     num_seeds = 3
     num_fidelity = 1
     num_objectives = 1
-    return {"dummy-task-1": TransferLearningTaskEvaluations(
-        config_space,
-        hyperparameters=pd.DataFrame(
-        [
-            {
-                "x": np.random.randint(0, 10),
-                "y": np.random.rand() / 2,
-                "z": np.random.choice(["a", "b"]),
-            }
-            for _ in range(num_evals)
-        ]
-        ),
-        objectives_evaluations=np.arange(num_evals * num_seeds * num_fidelity * num_objectives).reshape(num_evals, num_seeds, num_fidelity, num_objectives),
-        objectives_names=[metric])
+    return {
+        "dummy-task-1": TransferLearningTaskEvaluations(
+            config_space,
+            hyperparameters=pd.DataFrame(
+                [
+                    {
+                        "x": np.random.randint(0, 10),
+                        "y": np.random.rand() / 2,
+                        "z": np.random.choice(["a", "b"]),
+                    }
+                    for _ in range(num_evals)
+                ]
+            ),
+            objectives_evaluations=np.arange(
+                num_evals * num_seeds * num_fidelity * num_objectives
+            ).reshape(num_evals, num_seeds, num_fidelity, num_objectives),
+            objectives_names=[metric],
+        )
     }
-    
+
+
 def test_bounding_box():
 
     data = create_task_evaluation()
-    bb = BoundingBox(random_seed=random_seed, config_space=config_space, metric=metric,
-                     transfer_learning_evaluations=data,
-                     scheduler_fun=lambda new_config_space, metric, do_minimize, random_seed: SingleObjectiveScheduler(
-                         new_config_space,
-                         searcher="random_search",
-                         metric=metric,
-                         random_seed=random_seed,
-                         do_minimize=do_minimize,
-                     ),
-                     )
-    new_config_space = bb._compute_box(config_space=config_space, transfer_learning_evaluations=data,
-                                       num_hyperparameters_per_task=50, mode='min')
+    bb = BoundingBox(
+        random_seed=random_seed,
+        config_space=config_space,
+        metric=metric,
+        transfer_learning_evaluations=data,
+        scheduler_fun=lambda new_config_space, metric, do_minimize, random_seed: SingleObjectiveScheduler(
+            new_config_space,
+            searcher="random_search",
+            metric=metric,
+            random_seed=random_seed,
+            do_minimize=do_minimize,
+        ),
+    )
+    new_config_space = bb._compute_box(
+        config_space=config_space,
+        transfer_learning_evaluations=data,
+        num_hyperparameters_per_task=50,
+        mode="min",
+    )
 
-    assert new_config_space['x'].lower >= 0
-    assert new_config_space['x'].upper <= 10
-    assert new_config_space['y'].lower >= 0
-    assert new_config_space['y'].upper <=  0.5
-    assert new_config_space['z'].categories == ['a', 'b']
+    assert new_config_space["x"].lower >= 0
+    assert new_config_space["x"].upper <= 10
+    assert new_config_space["y"].lower >= 0
+    assert new_config_space["y"].upper <= 0.5
+    assert new_config_space["z"].categories == ["a", "b"]
