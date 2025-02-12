9ca7d8dd5dfcfca9ffae49d634b60425ee52dcc5
David  Salinas <david.salinas.pro@gmail.com>
2025-02-12 16:53:28 +0100

    Add benchmarking example with slurmpilot and jobarray

--- benchmarking/nursery/benchmark_conformal/launch_slurmpilot.py
+++ benchmarking/nursery/benchmark_conformal/launch_slurmpilot.py
@@ -1,103 +1,119 @@
 import os
-
 from argparse import ArgumentParser
-
 from pathlib import Path
 
 from coolname import generate_slug
-from tqdm import tqdm
-
-from syne_tune.util import random_string
-
 from slurmpilot.config import load_config
 from slurmpilot.slurm_wrapper import SlurmWrapper, JobCreationInfo
 from slurmpilot.util import unify
+from tqdm import tqdm
 
 from baselines import (
     methods,
     Methods,
 )
 
-
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument(
-        "--experiment_tag", type=str, required=False, default=generate_slug(2)
-    )
-    parser.add_argument(
-        "--experiment_subtag", type=str, required=False, default=random_string(4)
-    )
+    parser.add_argument("--experiment_tag", type=str, required=False, default="bench")
     parser.add_argument("--n_workers", type=int, required=False, default=1)
     parser.add_argument("--num_seeds", type=int, required=False, default=1)
+    parser.add_argument("--dry-run", action="store_true")
+
+    parser.add_argument("--cluster", type=str, required=True)
+    parser.add_argument("--partition", type=str, required=True)
+    parser.add_argument(
+        "--slurmpilot_folder", type=str, required=False, default="~/slurmpilot/jobs"
+    )
+
+    parser.add_argument("--sbatch_arguments", type=str, required=False)
+
     args, _ = parser.parse_known_args()
+
     experiment_tag = args.experiment_tag
-    experiment_subtag = args.experiment_subtag
+
     num_seeds = args.num_seeds
+    cluster = args.cluster
+    partition = args.partition
+    sbatch_arguments = args.sbatch_arguments
+    slurmpilot_folder = args.slurmpilot_folder
 
     print(args)
     print(f"Methods defined: {list(methods.keys())}")
     methods_selected = [
-        Methods.RS,
         Methods.LegacyRS,
-        Methods.REA,
         Methods.LegacyREA,
-        Methods.TPE,
-        Methods.LegacyTPE,
-        Methods.GP,
-        Methods.BORE,
+        # Methods.LegacyTPE,
         Methods.LegacyBORE,
-        Methods.BOTorch,
         Methods.LegacyBOTorch,
-        Methods.CQR,
         Methods.LegacyCQR,
-        # MF
-        Methods.ASHA,
         Methods.LegacyASHA,
-        Methods.ASHABORE,
         Methods.LegacyASHABORE,
-        Methods.ASHACQR,
         Methods.LegacyASHACQR,
-        Methods.BOHB,
         Methods.LegacyBOHB,
-        Methods.MOBSTER,
-        Methods.HYPERTUNE,
+        # Methods.RS,
+        # Methods.REA,
+        # Methods.TPE,
+        # Methods.GP,
+        # Methods.BORE,
+        # Methods.BOTorch,
+        # Methods.CQR,
+        # # MF
+        # Methods.ASHA,
+        # Methods.ASHABORE,
+        # Methods.ASHACQR,
+        # Methods.BOHB,
+        # Methods.MOBSTER,
+        # Methods.HYPERTUNE,
     ]
     print(f"{len(methods_selected)} methods selected: {methods_selected}")
 
     config = load_config()
-    cluster, partition = "kislurm", ""
     #    cluster, partition = 'scule', 'paul'
 
     slurm = SlurmWrapper(config=config, clusters=[cluster], ssh_engine="ssh")
-    max_runtime_minutes = 60 * 24
+    max_runtime_minutes = 60 * 4
     python_args = []
     for method in tqdm(methods_selected):
         assert method in methods
-        python_args.append(
-            {
-                "experiment_tag": experiment_tag,
-                "subtag": experiment_subtag,
-                "method": method,
-                "n_workers": args.n_workers,
-                "num_seeds": num_seeds,
-            }
-        )
+        for seed in range(num_seeds):
+            python_args.append(
+                {
+                    "method": method,
+                    "n_workers": args.n_workers,
+                    # TODO this runs only a given seed, the API of benchmark_main is quite ugly
+                    #  we could simplify this by only supporting `--seed N` as argument to benchmark_main.py
+                    "num_seeds": seed,
+                    "run_all_seed": 1,
+                }
+            )
 
+    jobname = unify(f"synetune/{experiment_tag}", method="date")
+    print(f"Going to launch {len(python_args)} jobs, jobname: {jobname}")
+    WANDB_API_KEY = os.environ.get("WANDB_API_KEY", "")
     jobinfo = JobCreationInfo(
         cluster=cluster,
         partition=partition,
-        jobname=unify("syne-tune/benchmarking", method="coolname"),
+        sbatch_arguments=sbatch_arguments,
+        jobname=jobname,
         entrypoint="benchmark_main.py",
         python_args=python_args,
         src_dir=str(Path(__file__).parent),
-        # python_libraries=["/home/aaron/git/syne-tune/syne_tune"],
         python_binary="python",
-        n_cpus=1,
+        python_libraries=[
+            str(Path(__file__).parent.parent.parent),
+            str(Path(__file__).parent.parent.parent.parent / "syne_tune"),
+        ],
+        n_cpus=8,
         mem=1024 * 8,
-        max_runtime_minutes=60 * 24,
-        bash_setup_command='eval "$(conda shell.bash hook)" && conda activate syne_tune',
+        max_runtime_minutes=max_runtime_minutes,
+        bash_setup_command="source ~/.bashrc; conda activate syne_tune",
         env={
             "WANDB_API_KEY": os.environ.get("WANDB_API_KEY", ""),
+            # write tuner files in Slurmpilot folder corresponding to `jobname`
+            "SYNETUNE_FOLDER": f"{slurmpilot_folder}/{jobname}",
         },
+        n_concurrent_jobs=100,  # max number of jobs to run at the same time
     )
-    jobid = slurm.schedule_job(jobinfo)
+    if not args.dry_run:
+        jobid = slurm.schedule_job(jobinfo)
