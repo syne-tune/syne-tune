40736343e7aa58bd7435acbda71a7bc74c056c68
Aaron Klein <kleiaaro@gmail.com>
2025-06-12 11:49:47 +0200

    Update syne_tune/optimizer/schedulers/searchers/last_value_multi_fidelity_searcher.py
    Co-authored-by: David Salinas <geoalgo@users.noreply.github.com>

--- syne_tune/optimizer/schedulers/searchers/last_value_multi_fidelity_searcher.py
+++ syne_tune/optimizer/schedulers/searchers/last_value_multi_fidelity_searcher.py
@@ -123,7 +123,7 @@ class LastValueMultiFidelitySearcher(SingleObjectiveBaseSearcher):
         self.searcher = self.searcher(
             config_space=self.config_space,
             # TODO BaseSearcher expects a int for random_seed, so we cannot pass a random state, we could change to pass both
-            random_seed=self.random_seed + self.random_state.randint(0, 2**31 - 1),
+            random_seed=self.random_seed + self.random_state.randint(0, sys.maxsize),
             points_to_evaluate=None,
             **self.searcher_kwargs,
         )
