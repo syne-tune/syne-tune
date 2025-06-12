267a0dd243e37e4131ee53c185e2cdb5a732a828
kavlahkaff <de28oser@studserv.uni-leipzig.de>
2025-06-12 16:34:39 +0200

    changed upload_on_hub to True

--- syne_tune/blackbox_repository/conversion_scripts/scripts/hpob_import.py
+++ syne_tune/blackbox_repository/conversion_scripts/scripts/hpob_import.py
@@ -645,4 +645,4 @@ if __name__ == "__main__":
 
     for recipe in recipes:
         instance = recipe()
-        instance.generate(upload_on_hub=False)
+        instance.generate(upload_on_hub=True)
