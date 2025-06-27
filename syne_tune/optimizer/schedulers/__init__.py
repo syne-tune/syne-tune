868bfd19c1e12ce95d6f208311d300eaa516dc25
Aaron Klein <kleiaaro@gmail.com>
2025-06-27 14:16:03 +0200

    imports

--- syne_tune/optimizer/schedulers/__init__.py
+++ syne_tune/optimizer/schedulers/__init__.py
@@ -1,24 +1,19 @@
-import logging
-
-from syne_tune.optimizer.schedulers.legacy_fifo import LegacyFIFOScheduler
-from syne_tune.optimizer.schedulers.legacy_hyperband import LegacyHyperbandScheduler
-from syne_tune.optimizer.schedulers.legacy_median_stopping_rule import (
-    LegacyMedianStoppingRule,
+from syne_tune.optimizer.schedulers.single_objective_scheduler import (
+    SingleObjectiveScheduler,
 )
+from syne_tune.optimizer.schedulers.single_fidelity_scheduler import (
+    SingleFidelityScheduler,
+)
+from syne_tune.optimizer.schedulers.median_stopping_rule import (
+    MedianStoppingRule,
+)
+from syne_tune.optimizer.schedulers.asha import AsynchronousSuccessiveHalving
 from syne_tune.optimizer.schedulers.pbt import PopulationBasedTraining
 
 __all__ = [
-    "LegacyFIFOScheduler",
-    "LegacyHyperbandScheduler",
-    "LegacyMedianStoppingRule",
+    "SingleFidelityScheduler",
+    "MedianStoppingRule",
+    "AsynchronousSuccessiveHalving",
+    "SingleObjectiveScheduler",
     "PopulationBasedTraining",
 ]
-
-try:
-    from syne_tune.optimizer.schedulers.ray_scheduler import (  # noqa: F401
-        RayTuneScheduler,
-    )
-
-    __all__.append("RayTuneScheduler")
-except ImportError as e:
-    logging.debug(e)
