24ac9e85c486ad660ac0fef5d0194f632ed2df5c
Aaron Klein <kleiaaro@gmail.com>
2025-06-27 14:39:43 +0200

    fix import

--- syne_tune/optimizer/schedulers/multiobjective/__init__.py
+++
@@ -1,9 +0,0 @@
-from syne_tune.optimizer.schedulers.multiobjective.moasha import MOASHA
-from syne_tune.optimizer.schedulers.multiobjective.multi_objective_regularized_evolution import (
-    MultiObjectiveRegularizedEvolution,
-)
-
-__all__ = [
-    "MOASHA",
-    "MultiObjectiveRegularizedEvolution",
-]
