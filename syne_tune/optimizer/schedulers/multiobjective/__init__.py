395a798b87fa23502002a6eccf8c9daaaa4a6021
Aaron Klein <kleiaaro@gmail.com>
2025-07-04 16:06:27 +0200

    multiobjective

--- syne_tune/optimizer/schedulers/multiobjective/__init__.py
+++ syne_tune/optimizer/schedulers/multiobjective/__init__.py
@@ -1,36 +1,15 @@
 import logging
 
-from syne_tune.optimizer.schedulers.multiobjective.legacy_moasha import LegacyMOASHA
+from syne_tune.optimizer.schedulers.multiobjective.moasha import MOASHA
 from syne_tune.optimizer.schedulers.multiobjective.multi_objective_regularized_evolution import (
     MultiObjectiveRegularizedEvolution,
 )
 from syne_tune.optimizer.schedulers.multiobjective.nsga2_searcher import (
     NSGA2Searcher,
 )
-from syne_tune.optimizer.schedulers.multiobjective.legacy_linear_scalarizer import (
-    LegacyLinearScalarizedScheduler,
-)
 
 __all__ = [
-    "LegacyMOASHA",
+    "MOASHA",
     "MultiObjectiveRegularizedEvolution",
     "NSGA2Searcher",
-    "LegacyLinearScalarizedScheduler",
 ]
-
-try:
-    from syne_tune.optimizer.schedulers.multiobjective.multi_surrogate_multi_objective_searcher import (  # noqa: F401
-        MultiObjectiveMultiSurrogateSearcher,
-    )
-    from syne_tune.optimizer.schedulers.multiobjective.random_scalarization import (  # noqa: F401
-        MultiObjectiveLCBRandomLinearScalarization,
-    )
-
-    __all__.extend(
-        [
-            "MultiObjectiveMultiSurrogateSearcher",
-            "MultiObjectiveLCBRandomLinearScalarization",
-        ]
-    )
-except ImportError as e:
-    logging.debug(e)
