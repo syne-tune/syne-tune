05504c5dd879f19f16062510859aa686fa03aa73
Aaron Klein <kleiaaro@gmail.com>
2025-07-04 16:26:36 +0200

    remove fifo, hyperband

--- syne_tune/optimizer/schedulers/multiobjective/__init__.py
+++ syne_tune/optimizer/schedulers/multiobjective/__init__.py
@@ -4,12 +4,8 @@ from syne_tune.optimizer.schedulers.multiobjective.moasha import MOASHA
 from syne_tune.optimizer.schedulers.multiobjective.multi_objective_regularized_evolution import (
     MultiObjectiveRegularizedEvolution,
 )
-from syne_tune.optimizer.schedulers.multiobjective.nsga2_searcher import (
-    NSGA2Searcher,
-)
 
 __all__ = [
     "MOASHA",
     "MultiObjectiveRegularizedEvolution",
-    "NSGA2Searcher",
 ]
